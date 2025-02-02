e25ce20e0230fd6beaea2835f516fa893353a476
Samir <118690585+sr5434@users.noreply.github.com>
2025-02-02 16:10:15 +0000

    Add "Study something else" button to dashboard for improved navigation

--- app/dashboard/[id]/page.jsx
+++ app/dashboard/[id]/page.jsx
@@ -46,6 +46,7 @@ export default function Dashboard({ params }) {
           >
             Copy session URL
           </a>
+          <a href="/" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Study something else</a>
           <div className="flex flex-row ml-5">
             <a href={`/open-ended/${id}`} className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Open-Ended</a>
             <a href={`/multiple-choice/${id}`} className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Multiple Choice</a>
